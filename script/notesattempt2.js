e1dd6dfd84e50dcbe3e5c5a3ea06dd56667cff17
EVE JOYCE <103681990@student.swin.edu.au>
2021-05-04 12:23:36 +1000

    finished 1 attempt with loops, started trying to just select each element

---
+++ script/notesattempt2.js
@@ -0,0 +1,72 @@
+var currentnote="";
+var notes = new Array();
+var category = new Array();
+
+category.push('cat1');
+category.push('cat2');
+category.push('cat3');
+
+function addcategory(element, index){
+    currentnote+='<option value="'+element+'">'+element+'</option>';
+    console.log('element= '+element+'index= '+index);
+}
+
+category.forEach(addcategory);
+
+console.log(document.getElementById("category"));
+console.log(document.getElementById("output"));
+
+document.getElementById('category').innerHTML+=currentnote;
+
+function addListTag(element, index){
+    document.getElementById("output").innerHTML+='<div id="noteContainer' + index + '"><span id="note' + index + '">' + element +"</span>" +
+                ' <button id="complete' + index + 'button" onClick="complete('+"'"+ index + "'"+')">complete item </button>' +
+                ' <button id="note' + index +  'button" onClick="remove_item('+ index + ')">remove item </button>' +"</div>";
+    console.log(document.getElementById("output"));
+}
+
+function add_item(){
+    var x = document.getElementById("form1");
+    notes.push(x.elements[0].value);
+    addListTag(notes[notes.length-1],notes.length);
+
+    console.log(notes);
+
+}
+
+function remove_item(index){
+    // if(index=notes.length){
+    //     notes.pop;
+    // } else {
+    notes.splice(index-1,1);
+    // }
+
+    document.getElementById('output').removeChild(document.getElementById('noteContainer'+index))
+
+    // the indexes on the html items don't get updated to match notes array so it breaks..
+
+    document.getElementById('output')
+    notes.forEach()
+
+    console.log(notes);
+
+}
+
+function clearList(){
+    notes = [];
+    document.getElementById("output").innerHTML="";
+}
+
+function complete(id){
+    // console.log(document.getElementById('note'+id).style.textDecorationLine);
+    
+    if(document.getElementById('note'+id).style.textDecorationLine==''){
+        document.getElementById('note'+id).style.textDecorationLine = "line-through";
+        document.getElementById('complete'+id+'button').innerHTML='uncomplete item';
+    }
+    else{
+        document.getElementById('note'+id).style.textDecorationLine = '';
+        document.getElementById('complete'+id+'button').innerHTML='complete item';
+    }
+
+}
